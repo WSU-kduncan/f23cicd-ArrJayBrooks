8e57402af3b3fd16cdd55ff7e063b9c408cb57e7
ArrJayBrooks <brooks.243@wright.edu>
2023-12-18 23:19:13 -0500

    Minor script changes

--- deployment/script.sh
+++ deployment/script.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/bash
 sudo docker rmi arrjaybrooks/f23cicd:latest
 sudo docker pull arrjaybrooks/f23cicd:latest
-sudo docker run -d -p 80:80 arrjaybrooks/f23cicd:latest
+sudo docker run -d -p 80:80 --name webapp --restart unless-stopped arrjaybrooks/f23cicd:latest
